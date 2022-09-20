a587e33c3af4e3eca33c2fd5b2d18e18ca953b72
Minyan Li <65792069+MinyanLi@users.noreply.github.com>
2022-09-20 23:33:24 +0200

    Update solution_3.py

--- 412 Fizz Buzz/solution_3.py
+++ 412 Fizz Buzz/solution_3.py
@@ -1,4 +1,9 @@
-# Return sends a specified value back to its caller whereas Yield can produce a sequence of values. We should use yield when we want to iterate over a sequence, but don’t want to store the entire sequence in memory. Yield is used in Python generators. A generator function is defined just like a normal function, but whenever it needs to generate a value, it does so with the yield keyword rather than return. If the body of a def contains yield, the function automatically becomes a generator function. 
+# Return sends a specified value back to its caller whereas Yield can produce a sequence of values. 
+# We should use yield when we want to iterate over a sequence, but don’t want to store the entire sequence in memory. 
+# Yield is used in Python generators. 
+# A generator function is defined just like a normal function, but whenever it needs to generate a value, 
+# it does so with the yield keyword rather than return. 
+# If the body of a def contains yield, the function automatically becomes a generator function. 
 
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
