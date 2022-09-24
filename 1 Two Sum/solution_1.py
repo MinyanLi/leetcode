1d164e1bd955a95a26ad3e94babe39ff6dda3c4a
Minyan Li <65792069+MinyanLi@users.noreply.github.com>
2022-09-24 18:53:26 +0200

    Create solution_1.py

---
+++ 1 Two Sum/solution_1.py
@@ -0,0 +1,9 @@
+# this solution will exceed time limit if the input data is too big
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i < j:
+                    if nums[i] + nums[j] == target:
+                        return i,j
