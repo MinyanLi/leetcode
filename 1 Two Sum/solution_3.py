96d8eca66541d5e6257c0ae65fc883dbfd3a3f6c
Minyan Li <65792069+MinyanLi@users.noreply.github.com>
2022-09-24 19:16:37 +0200

    Create solution_3.py

---
+++ 1 Two Sum/solution_3.py
@@ -0,0 +1,11 @@
+# 136 ms	15.4 MB
+# put the target - num item subtraction result in to a dictionary with index
+# look for matching
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        table = {}
+        for index in range(len(nums)):
+            if nums[index] in table:
+                return [table[nums[index]], index]
+            table[target - nums[index]] = index    # table = { (target - nums[index]): index}
