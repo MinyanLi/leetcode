277e5015d700155980959dc39bdf68de11f32058
Minyan Li <65792069+MinyanLi@users.noreply.github.com>
2022-10-19 09:33:15 +0200

    Create solution_1.py

---
+++ 3 Longest Substring Without Repeating Characters/solution_1.py
@@ -0,0 +1,23 @@
+# brutely iterate every substring
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+      
+        # check whether there's duplicate character in a substring
+        def check(start, end):
+            chars = set()
+            for i in range(start, end + 1):
+                c = s[i]
+                if c in chars:
+                    return False
+                chars.add(c)
+            return True
+
+        n = len(s)
+
+        res = 0
+        for i in range(n):
+            for j in range(i, n):
+                if check(i, j):
+                    res = max(res, j - i + 1)
+        return res
