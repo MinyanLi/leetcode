f734bdeef689d1d940e337bebba56fcee212398d
Minyan Li <65792069+MinyanLi@users.noreply.github.com>
2022-11-07 07:55:16 +0100

    Update solution_1.py

--- 0010_Regular Expression Matching/solution_1.py
+++ 0010_Regular Expression Matching/solution_1.py
@@ -1,6 +1,8 @@
 # Runtime 103 ms Beats 50.75%
 # Memory 13.9 MB Beats 91.16%
 
+# I guess it would be more difficult to write in other languages
+
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         import re
